b26ed79f9b2007b4038515e8d48b54c25021d612
Hung Pham <jeptoong@gmail.com>
2020-03-25 23:25:08 +0700

    Add some code

---
+++ src/main/java/org/tool/checkin/http/HttpHeaders.java
@@ -0,0 +1,105 @@
+package org.tool.checkin.http;
+
+import java.util.*;
+
+public class HttpHeaders implements Map<String, List<String>> {
+
+    public static final String CONTENT_TYPE = "Content-Type";
+
+    private final Map<String, List<String>> headers;
+
+    private final boolean readOnly;
+
+    public HttpHeaders() {
+        this(new LinkedHashMap<>(), false);
+    }
+
+    private HttpHeaders(Map<String, List<String>> headers, boolean readOnly) {
+        if (readOnly) {
+            Map<String, List<String>> map = new LinkedHashMap<>(headers);
+            headers.forEach((key, valueList) -> map.put(key, Collections.unmodifiableList(valueList)));
+            this.headers = Collections.unmodifiableMap(map);
+        } else {
+            this.headers = headers;
+        }
+        this.readOnly = readOnly;
+    }
+
+    @Override
+    public int size() {
+        return this.headers.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.headers.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return this.headers.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return this.headers.containsValue(value);
+    }
+
+    @Override
+    public List<String> get(Object key) {
+        return this.headers.get(key);
+    }
+
+    @Override
+    public List<String> put(String key, List<String> value) {
+        return this.headers.put(key, value);
+    }
+
+    @Override
+    public List<String> remove(Object key) {
+        return this.headers.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends List<String>> m) {
+        this.headers.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        this.headers.clear();
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return this.headers.keySet();
+    }
+
+    @Override
+    public Collection<List<String>> values() {
+        return this.headers.values();
+    }
+
+    @Override
+    public Set<Entry<String, List<String>>> entrySet() {
+        return this.headers.entrySet();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        HttpHeaders that = (HttpHeaders) o;
+        return readOnly == that.readOnly &&
+                Objects.equals(headers, that.headers);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.headers.hashCode();
+    }
+}
---
+++ src/main/java/org/tool/checkin/utils/Caller.java
@@ -0,0 +1,55 @@
+package org.tool.checkin.utils;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class Caller {
+
+    public static void doWithRequest() throws IOException {
+        String u1 = "https://api.checkin.bap.jp/api/rpc";
+        String u2 = "https://api.identity.bap.jp/api/rpc";
+        URL url = new URL(u2);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("action", "get_oauth_token");
+        data.put("grantType", "password");
+        data.put("grantData", Arrays.asList("hungp@bap.jp", "112qwaszx!"));
+        String input = "{\"action\": \"get_oauth_token\", \"grantType\": \"password\", \"grantData\": [\"hungp@bap.jp\", \"112qwaszx!\"]}";
+
+        JSONObject object = new JSONObject(data);
+        System.out.println(object.toString());
+        OutputStream os = conn.getOutputStream();
+        os.write(object.toString().getBytes());
+        os.flush();
+
+//            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+//                throw new RuntimeException("Failed : HTTP error code : "
+//                        + conn.getResponseCode());
+//            }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                (conn.getInputStream())));
+
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+
+        conn.disconnect();
+    }
+}
